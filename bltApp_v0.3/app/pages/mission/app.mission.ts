2396baf16ba35311703e55e64bb9a760ad8f4e91
Sorsly <han300@purdue.edu>
2016-08-22 18:50:59 -0400

    icons, mission page
    -resized icon size
    -added html/scss for mission page
    -made scss ids page specific

--- bltApp_v0.3/app/pages/mission/app.mission.ts
+++ bltApp_v0.3/app/pages/mission/app.mission.ts
@@ -10,8 +10,14 @@ export class Mission implements OnInit,OnDestroy{
     zMission="";
     hMissionS="";
     zMissionS="";
-    mission="buttsMission";
-    missionStatus="butt is being a butt";
+    background="\
+    After successfully planting charges around the hive cluster,\
+ the Resistance dealt a heavy blow to the Horde. Now crippled,\
+the Horde is more vulnerable than ever. To secure our advantage,\
+we'll need to establish an uplink to the quantum server core and RAM\
+their MOBO. ";
+    oTot1=5;
+    oCom1=2;
 
     constructor(private navParams: NavParams){
     }
